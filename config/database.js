ea153f040b4d889aa89a6152105c90ca31bb1634
EmanuelBastoss <164122474+EmanuelBastoss@users.noreply.github.com>
2024-10-15 21:04:17 -0400

    Atualiza o README e inclusão do dotenv

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('ApiGastos', 'usuario', '123', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+  }
+);
 
 module.exports = sequelize;
